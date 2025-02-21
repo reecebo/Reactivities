5dc111747df02eca59386b6f6e2f70e33229b1f7
Boland, Reece James <reboland@iu.edu>
2025-02-21 14:24:11 -0500

    End of section 2

---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,15 @@
+using System;
+using Domain;
+using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence;
+
+//App DbContext is used to query and save data to the SQLite database
+//This Class extends DbContext which is the main class in EF Core for working with a database
+//DbContextOptions allows configuration of the database connection and settings
+public class AppDbContext(DbContextOptions options): DbContext(options)
+{
+    //DbSet represents a row in the database where each row corresponds to an Activity entity (object) from the Domain namespace
+    public required DbSet<Domain.Activity> Activities { get; set; }
+}
