e852c3f0788de81f9d1d32bb18fb9262901aee39
Boland, Reece James <reboland@iu.edu>
2025-02-28 16:14:55 -0500

    End of section 6

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,28 +1,20 @@
-import { Box, Container, CssBaseline} from "@mui/material";
-import axios from "axios";
-import {useEffect, useState } from "react";
+import { Box, Container, CssBaseline, Typography} from "@mui/material";
+import { useState } from "react";
 import NavBar from "./NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import { useActivities } from "../../lib/hooks/useActivities";
 
 //defines a functional component named App
 function App() {
-  //creates a state variable activities and a function setActivities to update it
-  //--The initial value of activities is an empty array []
-  const [activities, setActivities] = useState<Activity[]>([]);
+ 
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const [editMode, setEditMode] = useState(false);
+  const {activities,isPending} = useActivities();
 
-  //useEffect is a side effect hook that runs after the component renders. 
-  // --Here it is used to fetch data from an API
-  //the first .then statement converts the results of the fetch to JSON
-  //the second .then statement passes this JSON to setActivities which updates the activities state
-  useEffect(() => {
-    axios.get<Activity[]>('https://localhost:5001/api/activities')
-    .then(response => setActivities(response.data))
-  },[])
+  
 
   const handleSelectActivity = (id: string) => {
-    setSelectedActivity(activities.find(x => x.id === id));
+    setSelectedActivity(activities!.find(x => x.id === id));
   }
 
   const handleCancelSelectActivity = () => {
@@ -39,38 +31,27 @@ function App() {
     setEditMode(false);
   }
 
-  const handleSubmitForm = (activity: Activity) => {
-    if(activity.id) {
-      setActivities(activities.map(x => x.id === activity.id ? activity : x))
-    } else {
-      const newActivity = {...activity, id: activities.length.toString()}
-      setSelectedActivity(newActivity)
-      setActivities([...activities,newActivity])
-    }
-    setEditMode(false);
-  }
-
-  const handleDelete = (id: string) => {
-    setActivities(activities.filter(x => x.id !== id))
-  }
   //the empty dependency array [] tells React to run this effect only once
 
   return (
-    <Box sx={{bgcolor: '#eeeeee'}}>
+    <Box sx={{bgcolor: '#eeeeee', minHeight: '100vh'}}>
       <CssBaseline />
       <NavBar openForm={handleOpenForm}/>
       <Container maxWidth='xl' sx={{mt: 3}}>
-        <ActivityDashboard 
-          activities= {activities}
-          selectActivity={handleSelectActivity}
-          cancelSelectActivity ={handleCancelSelectActivity}
-          selectedActivity={selectedActivity}
-          editMode={editMode}
-          openForm={handleOpenForm}
-          closeForm={handleFormClose}
-          submitForm={handleSubmitForm}
-          deleteActivity={handleDelete}
+        {!activities || isPending ? (
+          <Typography>Loading...</Typography>
+        ) : (
+          <ActivityDashboard 
+            activities= {activities}
+            selectActivity={handleSelectActivity}
+            cancelSelectActivity ={handleCancelSelectActivity}
+            selectedActivity={selectedActivity}
+            editMode={editMode}
+            openForm={handleOpenForm}
+            closeForm={handleFormClose}
         />
+        )}
+        
       </Container>
       
     </Box>
--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -11,8 +11,6 @@ type Props = {
     openForm: (id: string) => void;
     closeForm: () => void;
     editMode: boolean
-    submitForm: (activity: Activity) => void;
-    deleteActivity: (id: string) => void;
 }
 
 export default function ActivityDashboard({activities, cancelSelectActivity,
@@ -20,9 +18,7 @@ export default function ActivityDashboard({activities, cancelSelectActivity,
   selectedActivity,
   openForm,
   closeForm,
-  editMode,
-  submitForm,
-  deleteActivity
+  editMode
   
 }: Props) {
   return (
@@ -31,13 +27,12 @@ export default function ActivityDashboard({activities, cancelSelectActivity,
             <ActivityList 
               activities={activities}
               selectActivity={selectActivity}
-              deleteActivity={deleteActivity}
             />
         </Grid2>
         <Grid2 size={5}>
             {selectedActivity && !editMode && 
             <ActivityDetails
-              activity={selectedActivity} 
+              selectedActivity={selectedActivity} 
               cancelSelectActivity ={cancelSelectActivity}
               openForm={openForm}
             />
@@ -46,7 +41,6 @@ export default function ActivityDashboard({activities, cancelSelectActivity,
           <ActivityForm 
               closeForm={closeForm} 
               activity={selectedActivity}
-              submitForm={submitForm}
           
           />}
         </Grid2>
